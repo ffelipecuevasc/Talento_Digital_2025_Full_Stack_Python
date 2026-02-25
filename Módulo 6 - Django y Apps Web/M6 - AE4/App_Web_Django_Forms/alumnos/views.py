e8e9e8650984cb25fbdcf9601b6a195cba6df061
Felipe Cuevas <ffelipecuevasc@gmail.com>
2026-02-25 20:26:17 -0300

    Trigésimonoveno Commit - Subida de proyecto del Módulo 6 AE4 de Formularios Django

---
+++ Módulo 6 - Django y Apps Web/M6 - AE4/App_Web_Django_Forms/alumnos/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from . import forms
+
+# Create your views here.
+def portal_alumnos(request):
+    return render(request, 'alumnos/inicio.html')
+
+def lista_alumnos(request):
+    # Acá nosotros simulamos la consulta de datos a la BD (MySQL)
+    # Guardamos los datos en una LISTA de Python
+    # Acá hacemos un "SELECT * FROM ALUMNOS;"
+    alumnos = ["Chuck Norris", "Steven Seagal", "Jean Claude Van Damm", "Bruce Willis"]
+
+    # Luego, creamos un DICCIONARIO llamado contexto (context) que almacena todos los datos de BD
+    contexto = {
+        'lista_alumnos' : alumnos
+    }
+    # Enviamos lo necesario a el TEMPLATE (request [solicitud HTTP], recurso [HTML], diccionario [contexto])
+    return render(request, 'alumnos/lista_alumnos.html', contexto)
+
+def nuevo_alumno(request):
+    if request.method == 'GET':
+        # Acá llegaría cuando la persona apretó el botón de la barra de navegación, buscando el link
+        form = forms.AlumnoForm()
+    else:
+        # Acá llegaría cuando la persona apretó el botón del formulario 'Crear Alumno'
+        form = forms.AlumnoForm(request.POST)
+        if form.is_valid():
+            # Extraemos los datos de la solicitud HTTP (request) y los asignamos en variables
+            nombre = form.cleaned_data['nombre']
+            apellido = form.cleaned_data['apellido']
+            email = form.cleaned_data['correo_electronico']
+            contexto_post = {
+                'nombre' : nombre,
+                'apellido' : apellido,
+                'email' : email
+            }
+
+            return render(request, 'alumnos/registro_exito.html', contexto_post)
+    contexto = {
+        'form' : form
+    }
+    return render(request, 'alumnos/registro_alumno.html', contexto)
+
+
+
+
+
+
+
+
+
